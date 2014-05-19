633c755efd7cb5d8a7c9936b8e21c5c940f767af
guotingchaopr <guotingchaopr@gmail.com>
2014-05-19 23:53:09 +0800

    commit transfers

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var restify = require('restify');
-
+var ip_addr = '127.0.0.1';
+var port = '80'
 var server = restify.createServer({
-    name: "montiBigScreen",
-
+	name: "MonitoringServer"
 });
 
+var errors = function (req, res, next) {
+	return next(new restify.ConflictError("i just don't like u"));
+}
 
-server.listen(8080, function () {
-    console.log('%s listening at %s', server.name, server.url);
+server.get("/transfers", function (req, res, next) {
+	var transfers = [];
+	for (var i = 0; i < 12; i++) {
+		transfers.push(Math.ceil(Math.random() * 9999 + 1));
+	}
+	res.set("Access-Control-Allow-Origin", "*");
+	res.json(transfers);
+});
+
+server.get(/[\s\S]*/, errors);
+server.listen(port, ip_addr, function () {
+	console.log('%s listening at %s', server.name, server.url);
 });
